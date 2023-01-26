56b9433fa03a88f6b15cfbba0412fc8f3c625b94
Rasmus Schlunsen <rasmus@schlunsen.com>
2023-01-26 23:22:11 +0100

    Added npub1 formatting of pub keys. Added error msgs

--- src/mnemonic.rs
+++ src/mnemonic.rs
@@ -1,6 +1,8 @@
 use std::process::exit;
 
 use crate::cli::CLIArgs;
+use bech32::{ToBase32, Variant};
+use bitcoin_hashes::hex::ToHex;
 use nostr_sdk::prelude::{FromMnemonic, GenerateMnemonic, Keys, ToBech32};
 
 pub fn handle_mnemonic(parsed_args: &CLIArgs) {
@@ -9,9 +11,22 @@ pub fn handle_mnemonic(parsed_args: &CLIArgs) {
         if parsed_args.word_count <= 0 || parsed_args.word_count > 24 {
             word_count = 12;
         }
+        let mnemonic = Keys::generate_mnemonic(word_count).expect("Couldn't not generate mnemonic");
+        let keys = Keys::from_mnemonic(mnemonic.to_string(), None).expect("");
+
+        let pub_key_as_hex =
+            hex::decode(keys.public_key().to_hex()).expect("Error decoding public key to hex");
+        let nostr_pubkey = bech32::encode("npub", pub_key_as_hex.to_base32(), Variant::Bech32)
+            .expect("Error encoding to bech32");
+
         println!(
-            "Mnemonic: {}",
-            Keys::generate_mnemonic(word_count).expect("")
+            "Mnemonic: {}\nPublic key: {}\nPrivate key: {}",
+            mnemonic,
+            nostr_pubkey,
+            keys.secret_key()
+                .expect("Could not get secret key")
+                .to_bech32()
+                .expect("Could not get secret key bech32 conversion")
         );
         exit(0)
     }
@@ -21,11 +36,20 @@ pub fn handle_mnemonic(parsed_args: &CLIArgs) {
             parsed_args.mnemonic.to_string(),
             Some(parsed_args.mnemonic_passphrase.to_string()),
         )
-        .expect("");
+        .expect("Error creating key pair from mnemonic");
+
+        let pub_key_as_hex =
+            hex::decode(keys.public_key().to_hex()).expect("Error decoding public key to hex");
+        let nostr_pubkey = bech32::encode("npub", pub_key_as_hex.to_base32(), Variant::Bech32)
+            .expect("Error encoding to bech32");
+
         println!(
             "Public key: {}\nPrivate key: {}",
-            keys.public_key(),
-            keys.secret_key().expect("").to_bech32().expect("")
+            nostr_pubkey,
+            keys.secret_key()
+                .expect("Could not get secret key")
+                .to_bech32()
+                .expect("Could not get secret key bech32 conversion")
         );
         exit(0);
     }
